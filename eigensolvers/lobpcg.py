21bf516f8a720abe9ba76c8c74e4c43007aeec41
jamesETsmith <james.smith9113@gmail.com>
2024-11-02 14:44:05 -0400

    Adding batched LOBPCG

--- eigensolvers/lobpcg.py
+++ eigensolvers/lobpcg.py
@@ -57,15 +57,71 @@ def lobpcg(A: np.ndarray, X: np.ndarray, max_iter: int = 25, tol: float = 1e-5):
         Pi = np.einsum("ia,ab->ib", Si_orth[:, nx:], Ci[nx:, :nx])
 
 
-np.random.seed(18)
-N = 1000
-nx = 10
-H = np.random.rand(N, N)
-H = H + H.T
-psi = np.random.rand(N, nx)
+def batch_lobpcg(A: np.ndarray, X: np.ndarray, max_iter: int = 25, tol: float = 1e-5):
+    nx = X.shape[-1]
+    n_batches = X.shape[0]
+
+    # Solve initial rayleigh-ritz
+    Th, C = np.linalg.eigh(np.einsum("tia,tij,tjb->tab", X.conj(), A, X))
+
+    # Rotate the appro eigenvectors
+    Xi = np.einsum("tia,tab->tib", X, C)
+
+    # Residuals
+    Ri = np.einsum("tij,tja->tia", A, Xi) - np.einsum("tia,ta->tia", Xi, Th)
+
+    # Previous approximate eigenvectors
+    Pi = np.zeros_like(Xi)
+
+    for i in range(max_iter):
+        Wi = Ri
+        Si = np.concatenate([Xi, Wi, Pi], axis=2)
+        Si_orth = np.linalg.qr(Si)[0]  # (n_batches, , 3*nx)
+
+        shs = np.einsum("tia,tij,tjb->tab", Si_orth.conj(), A, Si_orth)
+        Thi, Ci = np.linalg.eigh(shs)
+
+        Xi = np.einsum("tia,tab->tib", Si_orth, Ci[:, :, :nx])
+        Ri = A @ Xi - np.einsum("tia,ta->tia", Xi, Thi[:, :nx])
+
+        if (np.linalg.norm(Ri, axis=(1, 2)) < tol).all():
+            print(f"Converged in {i} iterations")
+            break
+
+        Pi = np.einsum("tia,tab->tib", Si_orth[:, :, nx:], Ci[:, nx:, :nx])
+
+    return Thi[:, :nx], Xi
+
 
 np.set_printoptions(linewidth=160)
 
-lobpcg(H, psi, max_iter=50)
+# np.random.seed(18)
+# N = 128
+# nx = 10
+# H = np.random.rand(N, N)
+# H = H + H.T
+# psi = np.random.rand(N, nx)
+
+
+# lobpcg(H, psi, max_iter=50)
+# S_t, U_t = np.linalg.eigh(H)
+# print(S_t[:nx])
+
+
+np.random.seed(18)
+N = 128
+nx = 1
+n_batches = 1
+
+H = np.random.rand(n_batches, N, N)
+H = H + H.transpose(0, 2, 1)
+psi = np.random.rand(n_batches, N, nx)
+
+Thi, Xi = batch_lobpcg(H, psi, max_iter=50)
 S_t, U_t = np.linalg.eigh(H)
-print(S_t[:nx])
+
+print(f"LOBPCG eigenvalues {Thi}")
+print(f"Eigenvalues {S_t[:,:nx]}")
+
+fidelity = np.abs(np.einsum("tia,tia->t", Xi, U_t[:, :, :nx]))
+print(f"Fidelity of eigenvectors {fidelity}")

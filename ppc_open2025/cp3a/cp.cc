623b086ef75674fa8775d8d348b3ec848c457990
jamesETsmith <james.smith9113@gmail.com>
2025-03-15 13:34:21 -0400

    Small tweaks, some to preprocessing

--- ppc_open2025/cp3a/cp.cc
+++ ppc_open2025/cp3a/cp.cc
@@ -11,11 +11,22 @@ This is the function you need to implement. Quick reference:
 #include <numeric>
 #include <vector>
 
+//
+// User defined constants
+//
 int constexpr nb = 4; // size of vector
+int constexpr nd = 8; // size of blocks that we'll update in output
+
+//
+// User defined types
+//
 using double4_t = double __attribute__((vector_size(nb * sizeof(double))));
 
 constexpr double4_t d4zero = {0, 0, 0, 0};
 
+//
+// Helper functions
+//
 static inline double reduce_sqrt(double4_t x) {
   double result = 0;
   for (int i = 0; i < 4; i++) {
@@ -36,8 +47,6 @@ void correlate(int ny, int nx, const float *data, float *result) {
   // Calculate the number of vectors we'll need for each row
   int na = (nx + nb - 1) / nb;
 
-  // size of blocks that we'll update in output
-  int constexpr nd = 3;
   // number of blocks we'll need for each column
   int nc = (ny + nd - 1) / nd;
   // Number of rows after padding
@@ -45,78 +54,78 @@ void correlate(int ny, int nx, const float *data, float *result) {
 
   std::vector<double4_t> X(ncd * na, d4zero);
 
-// standardize the rows (set the mean to 0 and std to 1)
-#pragma omp parallel
-  {
-#pragma omp for
-    for (int y = 0; y < ny; y++) {
-      double mean = 0;
-      for (int x = 0; x < nx; x++) {
-        mean += data[x + y * nx];
-      }
-      mean /= nx;
-      double std = 0;
-      for (int x = 0; x < nx; x++) {
-        std += (data[x + y * nx] - mean) * (data[x + y * nx] - mean);
-      }
-      // Use the population standard deviation because we are using the entire
-      // dataset
-      std = sqrt(std / nx);
+  // standardize the rows (set the mean to 0 and std to 1)
 
-      for (int x = 0; x < nx; x++) {
-        X[x / nb + y * na][x % nb] = (data[x + y * nx] - mean) / std;
+#pragma omp parallel for
+  for (int y = 0; y < ny; y++) {
+    double mean = 0;
+    for (int x = 0; x < nx; x++) {
+      mean += data[x + y * nx];
+    }
+    mean /= nx;
+    double std = 0;
+    for (int x = 0; x < nx; x++) {
+      std += (data[x + y * nx] - mean) * (data[x + y * nx] - mean);
+    }
+    // Use the population standard deviation because we are using the entire
+    // dataset
+    std = sqrt(std / nx);
+
+    for (int x = 0; x < nx; x++) {
+      X[x / nb + y * na][x % nb] = (data[x + y * nx] - mean) / std;
+    }
+  }
+
+  // normalize the rows
+#pragma omp parallel for
+  for (int y = 0; y < ny; y++) {
+    double4_t sum = d4zero;
+    for (int xa = 0; xa < na; ++xa) {
+      sum += X[xa + y * na] * X[xa + y * na];
+    }
+    double norm = reduce_sqrt(sum);
+    double4_t norm_vec = {norm, norm, norm, norm};
+    for (int xa = 0; xa < na; ++xa) {
+
+      for (int xb = 0; xb < nb; ++xb) {
+        X[xa + y * na][xb] /= norm;
       }
     }
+  }
 
-    // normalize the rows
-#pragma omp for
-    for (int y = 0; y < ny; y++) {
-      double4_t sum = d4zero;
-      for (int xa = 0; xa < na; ++xa) {
-        sum += X[xa + y * na] * X[xa + y * na];
-      }
-      double norm = reduce_sqrt(sum);
-      for (int xa = 0; xa < na; ++xa) {
-        for (int xb = 0; xb < nb; ++xb) {
-          X[xa + y * na][xb] /= norm;
-        }
-      }
-    }
+  // compute XX^T
+#pragma omp parallel for schedule(dynamic)
+  for (int jc = 0; jc < nc; ++jc) {
+    for (int ic = jc; ic < nc; ++ic) {
 
-    // compute XX^T
-#pragma omp for schedule(dynamic)
-    for (int jc = 0; jc < nc; ++jc) {
-      for (int ic = jc; ic < nc; ++ic) {
+      std::array<std::array<double4_t, nd>, nd> vv = {d4zero};
+      std::array<double4_t, nd> xi;
+      std::array<double4_t, nd> yi;
 
-        std::array<std::array<double4_t, nd>, nd> vv = {d4zero};
-        std::array<double4_t, nd> xi;
-        std::array<double4_t, nd> yi;
+      for (int ka = 0; ka < na; ++ka) {
+        // indices are a mess here
+        // (unpacked row idx) * na + vector idx
 
-        for (int ka = 0; ka < na; ++ka) {
-          // indices are a mess here
-          // (unpacked row idx) * na + vector idx
-
-          for (int i = 0; i < nd; ++i) {
-            xi[i] = X[(jc * nd + i) * na + ka];
-            yi[i] = X[(ic * nd + i) * na + ka];
-          }
-
-          for (int i = 0; i < nd; ++i) {
-            for (int j = 0; j < nd; ++j) {
-              vv[i][j] += xi[i] * yi[j];
-            }
-          }
+        for (int i = 0; i < nd; ++i) {
+          xi[i] = X[(jc * nd + i) * na + ka];
+          yi[i] = X[(ic * nd + i) * na + ka];
         }
 
-        // reduce vv and store in result
-        for (int id = 0; id < nd; ++id) {
-          for (int jd = 0; jd < nd; ++jd) {
-            int i = ic * nd + id;
-            int j = jc * nd + jd;
+        for (int i = 0; i < nd; ++i) {
+          for (int j = 0; j < nd; ++j) {
+            vv[i][j] += xi[i] * yi[j];
+          }
+        }
+      }
 
-            if (i < ny && j < ny) {
-              result[i + j * ny] = reduce_d4t(vv[jd][id]);
-            }
+      // reduce vv and store in result
+      for (int id = 0; id < nd; ++id) {
+        for (int jd = 0; jd < nd; ++jd) {
+          int i = ic * nd + id;
+          int j = jc * nd + jd;
+
+          if (i < ny && j < ny) {
+            result[i + j * ny] = reduce_d4t(vv[jd][id]);
           }
         }
       }

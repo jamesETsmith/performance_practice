cc7bc3ea430dc1d1b5177589911268f6c128fb1f
jamesETsmith <james.smith9113@gmail.com>
2025-03-15 13:19:48 -0400

    Using loops unstead of manual unrolling

--- ppc_open2025/cp3a/cp.cc
+++ ppc_open2025/cp3a/cp.cc
@@ -46,77 +46,77 @@ void correlate(int ny, int nx, const float *data, float *result) {
   std::vector<double4_t> X(ncd * na, d4zero);
 
 // standardize the rows (set the mean to 0 and std to 1)
-#pragma omp parallel for
-  for (int y = 0; y < ny; y++) {
-    double mean = 0;
-    for (int x = 0; x < nx; x++) {
-      mean += data[x + y * nx];
-    }
-    mean /= nx;
-    double std = 0;
-    for (int x = 0; x < nx; x++) {
-      std += (data[x + y * nx] - mean) * (data[x + y * nx] - mean);
-    }
-    // Use the population standard deviation because we are using the entire
-    // dataset
-    std = sqrt(std / nx);
+#pragma omp parallel
+  {
+#pragma omp for
+    for (int y = 0; y < ny; y++) {
+      double mean = 0;
+      for (int x = 0; x < nx; x++) {
+        mean += data[x + y * nx];
+      }
+      mean /= nx;
+      double std = 0;
+      for (int x = 0; x < nx; x++) {
+        std += (data[x + y * nx] - mean) * (data[x + y * nx] - mean);
+      }
+      // Use the population standard deviation because we are using the entire
+      // dataset
+      std = sqrt(std / nx);
 
-    for (int x = 0; x < nx; x++) {
-      X[x / nb + y * na][x % nb] = (data[x + y * nx] - mean) / std;
-    }
-  }
-
-  // normalize the rows
-#pragma omp parallel for
-  for (int y = 0; y < ny; y++) {
-    double4_t sum = d4zero;
-    for (int xa = 0; xa < na; ++xa) {
-      sum += X[xa + y * na] * X[xa + y * na];
-    }
-    double norm = reduce_sqrt(sum);
-    for (int xa = 0; xa < na; ++xa) {
-      for (int xb = 0; xb < nb; ++xb) {
-        X[xa + y * na][xb] /= norm;
+      for (int x = 0; x < nx; x++) {
+        X[x / nb + y * na][x % nb] = (data[x + y * nx] - mean) / std;
       }
     }
-  }
 
-  // compute XX^T
-#pragma omp parallel for schedule(dynamic)
-  for (int jc = 0; jc < nc; ++jc) {
-    for (int ic = jc; ic < nc; ++ic) {
-
-      std::array<std::array<double4_t, nd>, nd> vv = {d4zero};
-
-      for (int ka = 0; ka < na; ++ka) {
-        // indices are a mess here
-        // (unpacked row idx) * na + vector idx
-        double4_t x0 = X[(jc * nd + 0) * na + ka];
-        double4_t x1 = X[(jc * nd + 1) * na + ka];
-        double4_t x2 = X[(jc * nd + 2) * na + ka];
-        double4_t y0 = X[(ic * nd + 0) * na + ka];
-        double4_t y1 = X[(ic * nd + 1) * na + ka];
-        double4_t y2 = X[(ic * nd + 2) * na + ka];
-
-        vv[0][0] += x0 * y0;
-        vv[0][1] += x0 * y1;
-        vv[0][2] += x0 * y2;
-        vv[1][0] += x1 * y0;
-        vv[1][1] += x1 * y1;
-        vv[1][2] += x1 * y2;
-        vv[2][0] += x2 * y0;
-        vv[2][1] += x2 * y1;
-        vv[2][2] += x2 * y2;
+    // normalize the rows
+#pragma omp for
+    for (int y = 0; y < ny; y++) {
+      double4_t sum = d4zero;
+      for (int xa = 0; xa < na; ++xa) {
+        sum += X[xa + y * na] * X[xa + y * na];
       }
+      double norm = reduce_sqrt(sum);
+      for (int xa = 0; xa < na; ++xa) {
+        for (int xb = 0; xb < nb; ++xb) {
+          X[xa + y * na][xb] /= norm;
+        }
+      }
+    }
 
-      // reduce vv and store in result
-      for (int id = 0; id < nd; ++id) {
-        for (int jd = 0; jd < nd; ++jd) {
-          int i = ic * nd + id;
-          int j = jc * nd + jd;
+    // compute XX^T
+#pragma omp for schedule(dynamic)
+    for (int jc = 0; jc < nc; ++jc) {
+      for (int ic = jc; ic < nc; ++ic) {
 
-          if (i < ny && j < ny) {
-            result[i + j * ny] = reduce_d4t(vv[jd][id]);
+        std::array<std::array<double4_t, nd>, nd> vv = {d4zero};
+        std::array<double4_t, nd> xi;
+        std::array<double4_t, nd> yi;
+
+        for (int ka = 0; ka < na; ++ka) {
+          // indices are a mess here
+          // (unpacked row idx) * na + vector idx
+
+          for (int i = 0; i < nd; ++i) {
+            xi[i] = X[(jc * nd + i) * na + ka];
+            yi[i] = X[(ic * nd + i) * na + ka];
+          }
+
+          for (int i = 0; i < nd; ++i) {
+            for (int j = 0; j < nd; ++j) {
+              vv[i][j] += xi[i] * yi[j];
+            }
+          }
+        }
+
+        // reduce vv and store in result
+        for (int id = 0; id < nd; ++id) {
+          for (int jd = 0; jd < nd; ++jd) {
+            int i = ic * nd + id;
+            int j = jc * nd + jd;
+
+            if (i < ny && j < ny) {
+              result[i + j * ny] = reduce_d4t(vv[jd][id]);
+            }
           }
         }
       }
